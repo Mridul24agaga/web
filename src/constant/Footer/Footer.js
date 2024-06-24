b43b8820f414cc9c2fd0c9238feed81ccecf803d
Mridul24agaga <101731772+Mridul24agaga@users.noreply.github.com>
2024-06-24 18:30:27 +0530

    Update Footer.js

--- src/constant/Footer/Footer.js
+++ src/constant/Footer/Footer.js
@@ -28,56 +28,8 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 ld:grid-cols-4 gap-y-8 pt-20">
         <div className="lg:pr-8 text-center md:text-start ">
         <p className="text-base mt-4 text-slate-200 hover:text-white transition-all duration-300">With 8 years of web and app development skills, we have worked on 100+ Internet development projects. We are a part of BroLabs Innovation Pvt. Ltd., and we are 38 people working together as a team with just one goal: to take your business to a new level.</p>
-        </div>
-          <div className="mx-auto text-center md:text-start">
-              <div>
-                <h3 className="text-white text-xl font-medium mb-6">Quick Links</h3>
-              </div>
-              <ul className="list-disc list-inside list-none">
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/">Home</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/about">About Us</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/contact">Contact us</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/portfolio">Portfolio</Link>
-                </li>
-              </ul>
-            </div>
-          <div className="mx-auto text-center md:text-start">
-              <div>
-                <h3 className="text-white text-capital text-xl font-medium mb-6">
-                  Our Services
-                </h3>
-              </div>
-              <ul className=" list-disc list-inside list-none">
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/digital-marketing">Ditigal Marketing</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/ppc-service">PPC Service</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/smo-service">SMO Service</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/seo-service">SEO Service</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/social-marketing">Social Marketing</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/website-optimization">Website Optimization</Link>
-                </li>
-                <li className="my-2 text-sm text-slate-200 hover:text-white transition-all duration-300">
-                  <Link href="/services/web-development">Web Development</Link>
-                </li>
-              </ul>
-            </div>
+      
+       
 
           <div className="mx-auto text-center md:text-start">
             <div>
@@ -100,7 +52,7 @@ const Footer = () => {
                     className="text-sm text-white text-sbold flex items-center justify-center md:justify-start gap-2"
                     href="mailto:webdevstudio1@gmail.com"
                   >
-                  <FaEnvelope/> webdev@example.com
+                  <FaEnvelope/> lorem.com
                   </a>
                 </li>
                 <li className="mt-8">
